defaaeda1a75aaf14f487f02e0894607960ce855
blank <maco@blank.local>
2017-06-18 13:46:41 -0500

    Brick prefab, ball collisions, levelManager

--- Assets/Scripts/ball.cs
+++ Assets/Scripts/ball.cs
@@ -4,11 +4,38 @@ using UnityEngine;
 
 public class ball : MonoBehaviour {
 
+	public float speed = 30;
+	public Rigidbody2D rigidBody;
+
 	// Use this for initialization
 	void Start () {
-		
+		rigidBody = GetComponent<Rigidbody2D> ();
+		rigidBody.velocity = Vector2.up * speed;
 	}
-	
+
+	void OnCollisionEnter2D (Collision2D col) {
+		if (col.gameObject.name == "paddle") {
+			handlePaddleHit (col);
+		}
+
+		if (col.gameObject.name == "wallButtom") {
+			// Exit
+			Application.Quit ();
+		}
+
+		if (col.gameObject.tag == "brick") {
+			Destroy (col.collider.gameObject);
+		}
+	}
+
+	void handlePaddleHit (Collision2D col) {
+		float ballX = GetComponent<Transform> ().position.x;
+		float paddleX = col.gameObject.GetComponent<Transform> ().position.x;
+		float paddleW = col.collider.bounds.size.x;
+		float x = (ballX - paddleX) / paddleW;
+		rigidBody.velocity = new Vector2 (x, 1) * speed;
+	}
+
 	// Update is called once per frame
 	void Update () {
 		
---
+++ Assets/Scripts/levelManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelManager : MonoBehaviour {
+
+	public GameObject brickPrefab;
+	public Color[] colors;
+
+	// Use this for initialization
+	void Start () {
+		float startx = -51;
+		float starty = 10;
+		float xchange = 6;
+		float ychange = 4;
+		for (int i = 0; i < 5; i++) {
+			GameObject brick = Instantiate (brickPrefab, new Vector2(startx + (xchange * i), starty), Quaternion.identity);
+			brick.GetComponent<SpriteRenderer> ().color = Color.blue;//colors[Random.Range(0, colors.Length)];
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
